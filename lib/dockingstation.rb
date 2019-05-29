7833b36b15fbf6c59be594de4bafbf79848cea6c
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-05-29 17:24:20 +0100

    gets Bike from docking station

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,4 +1,6 @@
 class DockingStation
   def release_bike
+    bike = Bike.new
+    return bike
   end
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,7 +1,11 @@
 require 'dockingstation'
+require 'bike'
 
 describe 'DockingStation' do
   it 'release bike' do
     expect(DockingStation.new).to respond_to(:release_bike)
+  end
+  it 'gets bike' do
+    expect(DockingStation.new.release_bike).to be_instance_of(Bike)
   end
 end
