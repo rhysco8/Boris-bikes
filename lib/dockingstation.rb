e6e79ba20bcfa0453136edc33e3a1a668323625f
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-05-31 17:10:06 +0100

    adds dock method and returns bike in dock

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,6 +1,14 @@
+
+
 class DockingStation
+  attr_reader :bike
+
   def release_bike
     bike = Bike.new
     return bike
   end
+  def dock(bike)
+    @bike = bike
+  end
+  
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,14 +2,32 @@ require 'dockingstation'
 require 'bike'
 
 describe 'DockingStation' do
-  it 'release bike' do
-    expect(DockingStation.new).to respond_to(:release_bike)
-  end
-  it 'gets bike' do
-    expect(DockingStation.new.release_bike).to be_instance_of(Bike)
+  describe '#release_bike' do
+    it 'releases bike' do
+      docking_station = DockingStation.new
+      expect(docking_station.release_bike).to be_instance_of(Bike)
+    end
   end
   it 'tests if bike works' do
     bike = DockingStation.new.release_bike
     expect(bike.working?).to be true
+  end
+  describe '#dock(bike)' do
+    it "docks a bike" do
+      docking_station = DockingStation.new
+      bike = Bike.new
+      docked_bike = docking_station.dock(bike)
+      expect(docked_bike).to eq(bike)
+    end
+    it 'lets you dock a bike' do
+      docking_station = DockingStation.new
+      expect(docking_station).to respond_to(:dock).with(1).argument
+    end
+  end
+  it 'lets you see a bike in docking station' do
+    docking_station = DockingStation.new
+    docked_bike = Bike.new
+    docking_station.dock(docked_bike)
+    expect(docking_station.bike).to eq(docked_bike)
   end
 end
